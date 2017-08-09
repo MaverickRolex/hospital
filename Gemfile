58e9441770897049ddee3b6d36609dcbeff9764d
maverickrolex <roga.zero@gmail.com>
2017-08-09 11:27:15 -0500

    validate current user system admin for, new, edit, destroy

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'devise'
 #Use activeadmin for administrate processes
 gem 'activeadmin'
 
+gem 'pry'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,26 +1,63 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation
+  permit_params :user_full_name, :department_id, :boss_department, :email, 
+                :password, :password_confirmation, :sistem_manager
+
+  controller do
+    def new
+      if current_user.sistem_manager?
+        @user = User.new
+      else
+        flash[:error] = "You do not have permissions to perform this task"
+        redirect_to admin_users_path
+      end
+    end
+    def edit
+      if current_user.sistem_manager?
+        @user = User.find(params[:id])
+      else
+        flash[:error] = "You do not have permissions to perform this task"
+        redirect_to admin_users_path
+      end
+    end
+    def destroy
+      if current_user.sistem_manager?
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to admin_users_path
+      else
+        flash[:error] = "You do not have permissions to perform this task"
+        redirect_to admin_users_path
+      end
+    end
+  end
 
   index do
     selectable_column
     id_column
+    column :user_full_name
     column :email
+    column :department_id 
+    column :boss_department
     column :current_sign_in_at
     column :sign_in_count
-    column :created_at
-    actions
+    if current_user.sistem_manager?
+      actions
+    end
   end
 
+  filter :user_full_name
   filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
+  filter :boss_department
 
   form do |f|
     f.inputs do
+      f.input :user_full_name
+      f.input :department_id 
+      f.input :boss_department
       f.input :email
       f.input :password
       f.input :password_confirmation
+      f.input :sistem_manager
     end
     f.actions
   end
